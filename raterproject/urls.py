9a34c917c85e1e20e5a026f5f28a1b75c7065521
Jake Littleton <jakelittleton30@gmail.com>
2026-08-12 13:59:20 -0500

    url pathway file created

--- raterproject/urls.py
+++ raterproject/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from raterapi.views import UserViewSet, GameView, CategoryView, GameCategoryView, ReviewView
 
 router = routers.DefaultRouter(trailing_slash=False)
-
+router.register(r'games', GameView, 'game')
+router.register(r'categories', CategoryView, 'category')
+router.register(r'gamecategories', GameCategoryView, 'gamecategory')
+router.register(r'reviews', ReviewView, 'review')
 urlpatterns = [
     path('', include(router.urls)),
+    path('login', UserViewSet.as_view({'post': 'user_login'}), name='login'),
+    path('register', UserViewSet.as_view({'post': 'register_account'}), name='register'),
 ]
 
