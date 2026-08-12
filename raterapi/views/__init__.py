a9ecc6e567406f00b4a8ae079436669d5eb2c641
Jake Littleton <jakelittleton30@gmail.com>
2026-08-12 13:58:41 -0500

    views init created

---
+++ raterapi/views/__init__.py
@@ -0,0 +1,5 @@
+from .users import UserViewSet, UserSerializer
+from .games import GameView, GameSerializer
+from .categories import CategoryView, CategorySerializer
+from .gamecategories import GameCategoryView
+from .reviews import ReviewSerializer, ReviewView
